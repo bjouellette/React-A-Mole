9604a2309b9d96f4c3680a308b43967a02480a01
Brittany Ouellette <bjouellette1@gmail.com>
2022-07-14 22:04:49 -0500

    Add files via upload

---
+++ src/components/EmptySlot.js
@@ -0,0 +1,25 @@
+import { useEffect } from 'react'
+import MoleHill from '../molehill.png'
+
+function EmptySlot(props){
+    
+    useEffect(() => {
+        // Feel free to adjust this number as you see fit.
+        // It is your game to tune!
+        let randSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.toggle(true)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+
+    return (
+        <div>
+            <img style={{'width': '30vw'}}
+            alt="hill"
+            src={MoleHill} />
+        </div>
+    )
+}
+
+export default EmptySlot
